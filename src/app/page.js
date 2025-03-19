9894569993664f10db7e14cf2e370a5f02262cdf
Gamesrol <gs.gore@gmail.com>
2025-03-19 14:02:13 +0100

    commit 2

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main>
       <h1>¡Hola, mundo!</h1>
-      <p>Esta es una práctica de deploy con Vercel 2222222222222.</p>
+      <p>Esta es una práctica de deploy con Vercel.</p>
     </main>
   );
 }
